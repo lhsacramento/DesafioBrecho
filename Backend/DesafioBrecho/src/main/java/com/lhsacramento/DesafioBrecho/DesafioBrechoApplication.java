bd4473e328b71cf62b6f92ee29ee5696104b0de7
Luiz Henrique Sacramento <luizsacramento.tds@gmail.com>
2024-06-04 14:09:09 -0300

    configurando spring boot no backend

---
+++ Backend/DesafioBrecho/src/main/java/com/lhsacramento/DesafioBrecho/DesafioBrechoApplication.java
@@ -0,0 +1,13 @@
+package com.lhsacramento.DesafioBrecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBrechoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBrechoApplication.class, args);
+	}
+
+}
---
+++ Backend/DesafioBrecho/src/test/java/com/lhsacramento/DesafioBrecho/DesafioBrechoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhsacramento.DesafioBrecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBrechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
