ad4a7e19cdd928cacfcf2a96c48bfe9b8638f5e0
Luiz Henrique Sacramento <luizsacramento.tds@gmail.com>
2024-06-09 18:17:17 -0300

    fixed incorrect class name

--- Frontend/DesafioBrecho/src/App.tsx
+++ Frontend/DesafioBrecho/src/App.tsx
@@ -1,10 +1,21 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyles } from "./styles/globalStyle"
+import { defaultTheme } from "./styles/themes/default"
+
 function App() {
 
   return (
     <>
-      <p>
-        Hello World
-      </p>
+      <GlobalStyles />
+
+      <ThemeProvider theme={defaultTheme}>
+
+          <div>
+            <h1>Meu Brechó</h1>
+          </div>
+
+      </ThemeProvider>
+
     </>
   )
 }
---
+++ Frontend/DesafioBrecho/src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+    white: "#FFF",
+    black: "#000",
+    blueLight: "#00c6ff",
+    blue: "#006cff",
+    blueDark: "0009b6"
+} as const
